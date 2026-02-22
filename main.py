56eab0a3c0e56b2cd78e0150dc37e1e10699877b
narendraksah <narendraksah@gmail.com>
2026-02-22 13:38:36 +0000

    job search

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 from dotenv import load_dotenv
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
-from langchain_google_genai import ChatGoogleGenerativeAI
-from tavily import TavilyClient
 
 load_dotenv()
 
-tavily = TavilyClient()
-
-
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
-
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_tavily import TavilySearch
 
 llm = ChatGoogleGenerativeAI(temperature=0.0, model="gemini-2.5-flash-lite")
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("Hello from langchain-handson: react-search-agent!")
     result = agent.invoke(
-        {"messages": HumanMessage(content="What is the weather in Tokyo?")}
+        {
+            "messages": HumanMessage(
+                content="Search for 3 job postings for AI engineer using langchain in Banglore, India on linkedin and list their details."
+            )
+        }
     )
     print(result)
 
