b5fe3ddfba3f25e767c3368696899b0d2b26876e
narendraksah <narendraksah@gmail.com>
2026-02-22 14:49:45 +0000

    structured output using pydantic

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,9 +9,25 @@ from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
 
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(description="The Agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate answer"
+    )
+
+
 llm = ChatGoogleGenerativeAI(temperature=0.0, model="gemini-2.5-flash-lite")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
