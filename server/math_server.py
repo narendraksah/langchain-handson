65120704264771c1d94d8dfc5d7a7fac8ac258fa
narendraksah <narendraksah@gmail.com>
2026-03-07 01:14:54 +0000

    math and weather server created

---
+++ server/math_server.py
@@ -0,0 +1,15 @@
+from mcp.server import FastMCP
+
+mcp = FastMCP("Math")
+
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
